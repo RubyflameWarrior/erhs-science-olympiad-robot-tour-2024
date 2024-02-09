3acca7cff7db3f201910deff58a7c8d5aeaab9f5
RubyflameWarrior <104446839+RubyflameWarrior@users.noreply.github.com>
2024-02-08 19:26:11 -0500

    Create initial PID

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+basic.show_icon(IconNames.ANGRY)
+serial.redirect_to_usb()
+
+"""
+def on_every_interval():
+basic.show_number(1)
+loops.every_interval(500, on_every_interval)
+
 def on_forever():
-    if True:
-        pass
+serial.write_value("Pitch", input.acceleration(Dimension.X))
 basic.forever(on_forever)
-maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+"""
+
+def myPID(setpoint):
+    kP = 1
+    kD = 0
+
+    error = setpoint – maqueen.ultrasonic(0)
+    derivative = 0
+    prevError = 0
+    power = 0
+
+    while error > 2:
+        error = setpoint – maqueen.ultrasonic(0)
+        serial.write_value("Error", error)
+        derivative = error – prevError
+        prevError = error
+        power = error * kP + derivative * kD
+        maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CW, power)
+        basic.pause(15)
+
+myPID(10)
--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
+basic.showIcon(IconNames.Happy)
+serial.redirectToUSB()
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+loops.everyInterval(500, function on_every_interval() {
+    basic.showNumber(1)
+})
+basic.forever(function on_forever() {
+    serial.writeValue("Pitch", input.acceleration(Dimension.X))
 })
