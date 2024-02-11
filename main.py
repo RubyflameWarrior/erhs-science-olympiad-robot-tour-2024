dd66aec0cfaf63110fa42a942bdfd4ed8c87966f
RubyflameWarrior <104446839+RubyflameWarrior@users.noreply.github.com>
2024-02-10 22:39:45 -0500

    Complete Regionals Code

--- main.py
+++ main.py
@@ -1,32 +1,86 @@
-basic.show_icon(IconNames.ANGRY)
+basic.show_icon(IconNames.HAPPY)
 serial.redirect_to_usb()
+# basic.show_number(25 - maqueen.ultrasonic(PingUnit.CENTIMETERS))
+
+def drive(sets):
+    maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CW, 50)
+    pause(100 * sets)
+    maqueen.motor_stop(maqueen.Motors.ALL)
+
+def driveBack(sets):
+    maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CCW, 50)
+    pause(100 * sets)
+    maqueen.motor_stop(maqueen.Motors.ALL)
+
+def turn(direction, sets):
+    if direction == "left":
+        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+    else:
+        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+    pause(261 * sets)
+    maqueen.motor_stop(maqueen.Motors.ALL)
+
+drive(0.5)
+pause(50)
+turn("right", 1)
+pause(50)
+drive(2)
+pause(50)
+turn("left", 1)
+pause(50)
+drive(3)
+pause(50)
+driveBack(1)
+pause(50)
+turn("right", 1)
+pause(50)
+drive(1)
+# turn("right", 4)
 
 """
-def on_every_interval():
-basic.show_number(1)
-loops.every_interval(500, on_every_interval)
+PIDMode = 1
+sameErrorCount = 0
 
+setpoint = 10
+
+error = setpoint - maqueen.ultrasonic(PingUnit.CENTIMETERS)
+previousError = error
+derivative = 0
+power = 0
+
+def on_every_interval():
+    basic.show_number(PIDMode)
+    if PIDMode == 1:
+        kP = 5
+        kD = 0
+
+        error = setpoint - maqueen.ultrasonic(PingUnit.CENTIMETERS)
+        if error == previousError:
+            sameErrorCount = sameErrorCount + 1
+        derivative = error - previousError
+        previousError = error
+        power = (error * kP)
+        # basic.show_number(error)
+        if error > 0:
+            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CCW, abs(power))
+            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CCW, abs(power))
+        elif error < 0:
+            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, abs(power))
+            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, abs(power))
+loops.every_interval(10, on_every_interval)
+
+def basicDrive(target):
+    global setpoint = target
+    global PIDMode = 1
+    while sameErrorCount < 10:
+        pause(10)
+    global PIDMode = 0
+    sameErrorCount = 0
+
+# basicDrive(25)
 def on_forever():
 serial.write_value("Pitch", input.acceleration(Dimension.X))
 basic.forever(on_forever)
 """
-
-def myPID(setpoint):
-    kP = 1
-    kD = 0
-
-    error = setpoint – maqueen.ultrasonic(0)
-    derivative = 0
-    prevError = 0
-    power = 0
-
-    while error > 2:
-        error = setpoint – maqueen.ultrasonic(0)
-        serial.write_value("Error", error)
-        derivative = error – prevError
-        prevError = error
-        power = error * kP + derivative * kD
-        maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CW, power)
-        basic.pause(15)
-
-myPID(10)
--- main.ts
+++ main.ts
@@ -1,9 +1,90 @@
 basic.showIcon(IconNames.Happy)
 serial.redirectToUSB()
-maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
-loops.everyInterval(500, function on_every_interval() {
-    basic.showNumber(1)
-})
-basic.forever(function on_forever() {
-    serial.writeValue("Pitch", input.acceleration(Dimension.X))
-})
+//  basic.show_number(25 - maqueen.ultrasonic(PingUnit.CENTIMETERS))
+function drive(sets: number) {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    pause(100 * sets)
+    maqueen.motorStop(maqueen.Motors.All)
+}
+
+function driveBack(sets: number) {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+    pause(100 * sets)
+    maqueen.motorStop(maqueen.Motors.All)
+}
+
+function turn(direction: string, sets: number) {
+    if (direction == "left") {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+    } else {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+    }
+    
+    pause(261 * sets)
+    maqueen.motorStop(maqueen.Motors.All)
+}
+
+drive(0.5)
+pause(50)
+turn("right", 1)
+pause(50)
+drive(2)
+pause(50)
+turn("left", 1)
+pause(50)
+drive(3)
+pause(50)
+driveBack(1)
+pause(50)
+turn("right", 1)
+pause(50)
+drive(1)
+//  turn("right", 4)
+/** 
+PIDMode = 1
+sameErrorCount = 0
+
+setpoint = 10
+
+error = setpoint - maqueen.ultrasonic(PingUnit.CENTIMETERS)
+previousError = error
+derivative = 0
+power = 0
+
+def on_every_interval():
+    basic.show_number(PIDMode)
+    if PIDMode == 1:
+        kP = 5
+        kD = 0
+
+        error = setpoint - maqueen.ultrasonic(PingUnit.CENTIMETERS)
+        if error == previousError:
+            sameErrorCount = sameErrorCount + 1
+        derivative = error - previousError
+        previousError = error
+        power = (error * kP)
+        # basic.show_number(error)
+        if error > 0:
+            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CCW, abs(power))
+            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CCW, abs(power))
+        elif error < 0:
+            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, abs(power))
+            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, abs(power))
+loops.every_interval(10, on_every_interval)
+
+def basicDrive(target):
+    global setpoint = target
+    global PIDMode = 1
+    while sameErrorCount < 10:
+        pause(10)
+    global PIDMode = 0
+    sameErrorCount = 0
+
+# basicDrive(25)
+def on_forever():
+serial.write_value("Pitch", input.acceleration(Dimension.X))
+basic.forever(on_forever)
+
+ */
